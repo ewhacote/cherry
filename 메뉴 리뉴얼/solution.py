553c1e19d5638746f8bf2293651984392c32e7cf
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-09-12 23:13:08 +0900

    [solved] 메뉴리뉴얼

---
+++ 메뉴 리뉴얼/solution.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from itertools import combinations
+
+
+def solution(orders, course):
+    answer = []
+    for c in course:
+        tmp = []
+        for order in orders:
+            com = combinations(sorted(order), c)
+            tmp += com
+        counter = Counter(tmp)
+        print(counter)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(c) for c in counter if counter[c] == max(counter.values())]
+    return sorted(answer)
+
+solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4])
