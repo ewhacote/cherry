8a08c9e3c97de514469b07cacf63b91560325c7b
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-09-17 23:03:16 +0900

    [fix] 삼각 달팽이

--- 삼각 달팽이/solution.py
+++ 삼각 달팽이/solution.py
@@ -1,11 +1,12 @@
-directions = {0: (1, 0), 1: (0, 1), 2: (-1, 1)}
+directions = {0: (1, 0), 1: (0, 1), 2: (-1, -1)}
+
 
 def solution(n):
-    triangle_slug = [[0] * i for i in range(1,n+1)]
-    r, c, num = -1,0,1
+    triangle_slug = [[0] * i for i in range(1, n + 1)]
+    r, c, num = -1, 0, 1
     for i in range(n):
-        for _ in range(i,n):
-            dir_r, dir_c  = directions[i%3]
+        for _ in range(i, n):
+            dir_r, dir_c = directions[i % 3]
             r, c = r + dir_r, c + dir_c
             triangle_slug[r][c] = num
             num += 1
